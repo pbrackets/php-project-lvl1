95754c77cbb52d2bf959c1f12778b71788c3c695
Aelita <pbrackets@yandex.ru>
2022-08-17 16:12:25 +0500

    исправила ошибки_4

--- src/Engine.php
+++ src/Engine.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 const ROUNDS_COUNT = 3;
 
-function runGame(string $description, array $generateRound)
+function runGame(string $description, \Closure $generateRound)
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
