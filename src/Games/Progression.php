ebf8cd8c4c76730faf7c97cb5bd7355408a0127a
Aelita <pbrackets@yandex.ru>
2022-08-17 11:31:43 +0500

    brain-progression

---
+++ src/Games/Progression.php
@@ -0,0 +1,32 @@
+<?php
+namespace BrainGames\Games\Gcd;
+use function BrainGames\Engine\runGame;
+
+const DESCRIPTION = 'What number is missing in the progression?';
+const LENGTH_PROGRESSION = 10;
+
+function getProgression($length, $begin, $step): array
+{
+    $result = [];
+    for ($i = 0; $i < $length; $i++) {
+        $begin += $step;
+        $result[] = $begin;
+    }
+    return $result;
+}
+
+function run()
+{
+    $generateRound = function () {
+        $beginProgression = rand(1, 10);
+        $stepProgression = rand(10, 10);
+        $progression = getProgression(LENGTH_PROGRESSION, $beginProgression, $stepProgression);
+
+        $missingElement = rand(0, count($progression)-1);
+        $solution = $progression[$missingElement];
+        $progression[$missingElement] = '..';
+        $question = implode(' ', $progression);
+        return [$question, $solution];
+    };
+    runGame(DESCRIPTION, $generateRound);
+}
