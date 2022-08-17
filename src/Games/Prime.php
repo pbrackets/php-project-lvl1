b2de9fae22e0336d8406e9d64f7326e87405ddd9
Aelita <pbrackets@yandex.ru>
2022-08-17 12:33:22 +0500

    brain-prime

---
+++ src/Games/Prime.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BrainGames\Games\Prime;
+use function BrainGames\Engine\runGame;
+
+const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function run()
+{
+    $generateRound = function () {
+        $number = rand(1, 100);
+        $question = $number;
+        $flag = true;
+        for ($i = 2; $i < $number; $i++) {
+            if ($number % $i === 0) {
+                $flag = false; // если хотя бы один раз поделилось
+            }
+        }
+        if ($flag === false) {
+            $solution = 'no';
+        } else {
+            $solution = 'yes';
+        }
+
+        return [$question, $solution];
+    };
+    runGame(DESCRIPTION, $generateRound);
+}
